e3491fdfe3c095d34e73fceb21fa78474fec1bc9
Henrique_Jean <j.darkline.dev@pm.me>
2026-02-15 16:51:07 -0300

    feat/Confog-Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,34 @@
+import { transform } from 'typescript';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx},"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'lumina-bg': '#0a0a0a',
+        'lumina-card': '#171717',
+        'lumina-accent': '#6366f1',
+        'lumina-cyan': '#06b6d4'
+      },
+
+      fontFamily: {
+        'sans': ['Inter', 'sans-serif'],
+      },
+
+      animation: {
+        'float': 'float 6s ease-in-out infinite',
+      },
+
+      keyframes: {
+        float: {
+          '0%, 100%': {transform: 'translateY(0)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
