ec50ebdd6af07313e6616870b680ba18742ae661
Henrique_Jean <j.darkline.dev@pm.me>
2026-02-16 21:29:49 -0300

    feat/Update-Home-Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import { transform } from 'typescript';
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx},"
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
